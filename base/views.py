71039e47344e128850c4b0be0bd7db1dd3504199
alonzo245 <alon.alush.mail@gmail.com>
2022-09-25 10:40:53 +0300

    save

---
+++ base/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import Room, Topic
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+# from django.http import HttpResponse
+from .forms import RoomForm
+from django.contrib import messages
+
+# Create your views here.
+################################
+################################
+################################
+# 2:41:52
+################################
+################################
+
+rooms = [
+    {'id': 1, 'name': 'static room'}
+]
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'somthing went wrong....')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    rooms_count = rooms.count()
+    # return HttpResponse('Home Page')
+    return render(request, 'base/home.html', {'rooms': rooms, 'topics': topics, 'rooms_count': rooms_count})
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm
+
+    if request.method == 'POST':
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj': room})
+
+# def room(request, pk):
+#     room = None
+#     for i in rooms:
+#         if i['id'] == int(pk):
+#             room = i
+#     context = {'room':room}
+#     return render(request, 'base/room.html', context)
